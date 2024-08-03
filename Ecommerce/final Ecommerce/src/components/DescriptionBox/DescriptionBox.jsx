527fd68ce5a728c34d9c9107134c7d78fae792ce
alihaidari079 <149894041+alihaidari079@users.noreply.github.com>
2024-08-03 09:46:31 +0430

    Add files via upload

---
+++ Ecommerce/final Ecommerce/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descrip-navigator">
+      <div className="descrip-nav-box">Description</div>
+        <div className="descrip-nav-box fade">
+          Reviws (122)
+        </div>
+        </div>
+        <div className="descripbox-description">
+      <p>Welcome to E-commerce, your ultimate online destination for fashion-forward clothing for women, men, and kids. Our eCommerce platform is designed to offer a seamless, enjoyable shopping experience, bringing the latest trends and timeless styles directly to your fingertips. </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt distinctio voluptatibus corrupti ut enim natus exercitationem quos nesciunt fuga in amet blanditiis suscipit, aut animi autem fugiat molestias illo sapiente!</p>
+        </div>
+      
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
