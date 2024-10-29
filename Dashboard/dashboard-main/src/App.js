602f752ddc1704cd9385296df0d5dc1f8b8bab8a
alihaidari079 <149894041+alihaidari079@users.noreply.github.com>
2024-10-29 16:26:52 +0430

    Add files via upload

---
+++ Dashboard/dashboard-main/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {
+  const {theme, colorMode} = useState();
+  return (
+    <div className="App">
+    
+     
+    </div>
+  );
+}
+
+export default App;
